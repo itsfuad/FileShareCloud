e7a16fbf293f7051638c7ee455874732b7ab1bfa
Fuad Hasan <fuad.cs22@gmail.com>
2024-10-04 00:31:26 +0600

    ..

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,21 +3,13 @@
     import "$lib/fontawesome/css/all.min.css";
 
     import NavigationIndicator from "$lib/NavigationIndicator.svelte";
-    import { onMount } from "svelte";
-   
-    let loaded = false;
-
-    onMount(() => {
-        loaded = true;
-    });
 
 </script>
 
 
 <NavigationIndicator />
-{#if loaded}
+
 <slot></slot>
-{/if}
 
 <svelte:head>
     <title>File share Cloud</title>
